1578721f086648b9de7a0885c39a2a591ace9ac7
michals <michal.swierczewski@pl.ibm.com>
2016-11-30 14:43:14 +0100

    version_counter v1

---
+++ version_counter.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# min 0.1.0
+# max 9.9.9
+
+var = "1.9.9"
+s = var.split(".")
+f = []
+# if, first element is '0' for example '0.1.0'
+if var != "9.9.9":
+	if int(s[0]) == 0 and var != "0.9.9":
+		f += "0"
+		#print s[1:]
+		print str(int("".join(s[1:]))+1)
+		f += str(int("".join(s[1:]))+1)
+	elif var == "0.9.9" or 0 < int(s[0]) <= 9:
+		f += str(int("".join(var.split(".")))+1)
+	else:
+		print "no way!"
+else:
+	print "no way!"
+		
+print "f", f
+print "join", ".".join(f)
+
