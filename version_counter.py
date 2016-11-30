79f1697d86e4349c53f6d91f578b302da87e7b0e
michals <michal.swierczewski@pl.ibm.com>
2016-11-30 16:00:19 +0100

    working with version_counter.rb

--- version_counter.py
+++ version_counter.py
@@ -3,16 +3,16 @@
 # min 0.1.0
 # max 9.9.9
 
-var = "9.9.8"
+var = "0.9.8"
 s = var.split(".")
 f = []
 # if, first element is '0' for example '0.1.0'
 if var != "9.9.9":
 	if int(s[0]) == 0 and var != "0.9.9":
 		f += "0"
-		#print s[1:]
 		print str(int("".join(s[1:]))+1)
 		f += str(int("".join(s[1:]))+1)
+		print f
 	elif var == "0.9.9" or 0 < int(s[0]) <= 9:
 		f += str(int("".join(var.split(".")))+1)
 	else:
@@ -21,4 +21,3 @@ if var != "9.9.9":
 	print "join", ".".join(f)
 else:
 	print "no way!"
-
--- version_counter.rb
+++ version_counter.rb
@@ -17,4 +17,25 @@ read policy_version.txt if exist, check version and opaque, if new version/opaqu
 merge script with rake (not sure if needed)
 """
 
+var = "1.9.8"
+s = var.split(".")
+f = []
 
+# if, first element is '0' for example '0.1.0'
+if var != "9.9.9"
+	if s[0].to_i == 0 and var != "0.9.9"
+		f << "0"
+		p f
+		p s[1..-1]
+		new_s = s[1..-1].map { |k| "#{k}" }.join("")
+		p (new_s.to_i + 1).to_s
+		(((new_s.to_i + 1).to_s).split('')).each do |i|
+			f << i
+		end
+	elsif var == "0.9.9" or 0 < s[0].to_i <= 9
+		puts "test"
+	end
+	p f[0..-1].map { |k| "#{k}" }.join(".")
+else
+	p "no way!"
+end
