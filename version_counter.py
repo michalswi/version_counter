a53ee019e51a71c9a157ad1e0f7003cfc7ad8f65
michals <michal.swierczewski@pl.ibm.com>
2016-12-02 12:21:00 +0100

    py: if lock.json exist, check version&id_revision if not add to policy version file

--- version_counter.py
+++ version_counter.py
@@ -21,54 +21,64 @@ def version_counter(ver):
 			f += str(int("".join(ver.split(".")))+1)
 		else:
 			print "no way!"
-			sys.exit("bye!")
 		f = ".".join(f)
 		print f
 		return f
 	else:
-		print "end here, current version is 9.9.9"
-		sys.exit("bye!")
+		print "Can't proceed with version 9.9.9"
+		return ver
+		
 
 ## WORK WITH FILE
 # TO DO:
 # file empty
 # file not empty (under consideration if empty lines)
 
-policyfile_name = 'policy_version.txt'
-
 # EMPTY FILE
-def empty(filename):
-	# init vers & opaque for test, HOW TO READ OPAQUE??
+def empty(filename, id_revision):
+	print "empty function"
+	# initial version is always 0.1.0
 	vers = '0.1.0'
-	opa = 'a111'
 	with open(filename, 'r+') as ef:
-		ef.write(vers + ':' + opa + '\n')
+		ef.write(vers + ':' + id_revision + '\n')
 
 # NOT EMPTY FILE
-def not_empty(filename):
-	last_line = subprocess.check_output(['tail', '-1', filename])
-	vers, opa = last_line.strip().split(":")
-	with open(filename, 'a') as ef:
-		ef.write(version_counter(vers) + ':' + opa + '\n')
+# WHAT HAPPEN IF ONE OF THE POLICYFILES is 9.9.9 ? -> ef.write(version_counter(vers).... append the same line
+def not_empty(filename, id_revision):
+	print "not_empty function"
+	#last_line = subprocess.check_output(['tail', '-1', filename])
+	#vers, opa = last_line.strip().split(":")
+	with open(filename, 'a+') as policyver:
+		for i in policyver.readlines():
+			vers, opa = i.split(":")
+			if opa.strip() == id_revision:
+				print "For %s requested id_revision:%s already exist under version:%s" %(filename, opa.strip(), vers)
+				break
+			else:
+				policyver.write(version_counter(vers) + ':' + id_revision + '\n')
 
 # EMPTY OR NOT
-def empty_or_not(filename):
+def empty_or_not(filename, id_revision):
 	if os.path.getsize(filename) == 0:
-		print "empty"
-		empty(filename)
+		print "-> empty"
+		# ->
+		empty(filename, id_revision)
 	else:
-		print "not empty"
-		not_empty(filename)	
+		print "-> not empty"
+		# ->
+		not_empty(filename, id_revision)	
 
 # MAIN
-def main(filename):
+def main(filename,id_revision):
 	if os.path.isfile(filename):
-		empty_or_not(filename)
+		# ->
+		empty_or_not(filename, id_revision)
 	else:
 		print "{} doesn't exist but I created it, nice isn't it? ;]".format(filename)
 		# file with permission 664
 		subprocess.call(['touch', filename])
-		empty_or_not(filename)
+		# ->
+		empty_or_not(filename, id_revision)
 
 # VERIFY IF LOCK.JSON EXIST
 def if_json_exist():
@@ -88,7 +98,9 @@ def if_json_exist():
 		print "no lock.json!"
 		sys.exit()
 
-print if_json_exist()
-
 if __name__ == '__main__':
-	main(policyfile_name)
+	for i in if_json_exist().items():
+		main(i[0].split('.')[0], i[1])
+
+#policyfile_name = 'policy_version.txt'	
+#main(policyfile_name)
